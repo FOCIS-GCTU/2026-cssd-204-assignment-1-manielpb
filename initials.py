aa04473ced4f1443586a7a3c14b9b94b7dae2286
maniel <manuelpepr99@icloud.com>
2026-05-31 17:29:16 +0000

    Initial commit

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+#File: initials.py
+#Description: Print out my initials
+#Assignment Number: 1
+#
+#Name: Emmanuel Peprah Kwame
+#STUDENT ID: 2425401674
+#Email: 2425401674@live.gctu.edu.gh
+#Grader: Augustus
+#
+#On my honor, Emmanuel Peprah, this programming assignment is my own work 
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+  # Print my initials in large letters
 
+  print()
+  print("...EPK")
+  print()
+    
+  print("...EEEEEEEEEEEE........PPPPPPPPPPPP........KKKK...KKKKK.....")
+  print("...EEEEEEEEEEEE........PPPP....PPPP........KKKK..KKKK.......")
+  print("...EEEE................PPPP....PPPP........KKKK.KKK.........")
+  print("...EEEE................PPPP....PPPP........KKKKKK...........")
+  print("...EEEEEEEEEEEE........PPPPPPPPPPPP........KKKKK............")
+  print("...EEEEEEEEEEEE........PPPP................KKKKK............")
+  print("...EEEE................PPPP................KKKKKK...........")
+  print("...EEEE................PPPP................KKKK.KKK.........")
+  print("...EEEEEEEEEEEE...**...PPPP...........**...KKKK..KKKK.....**")
+  print("...EEEEEEEEEEEE...**...PPPP...........**...KKKK...KKKKK...**")
+
+  print()
 
 
 main()
